f224db5bdd673fdc1f3070c5108b45bdf8e830f1
AlesVoronin <alesworonin@gmail.com>
2024-06-11 23:10:33 +0300

    first commit

---
+++ src/api.jsx
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "6ea96d2845mshda4a7f7a16c6484p1f8dc7jsn8e376f2fe270",
+    "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
